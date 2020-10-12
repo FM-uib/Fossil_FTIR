0c435cbe8cdbb32812fd82a5180956f5cb2512d6
Florian <florian.muthreich@uib.no>
2020-10-12 11:32:37 +0200

    wrote data reading file; assigns unique ID; read DAL samples

--- R/read_data.R
+++ R/read_data.R
@@ -1,7 +1,23 @@
 library(here)
+library(stringi)
 
 raw = read.csv(here("data", "input", "spectra", "DAL_spectra_raw.csv"), check.names = F, colClasses = "character")
 
 spectra_raw = as.matrix((raw[-c(1,2),c(1:765)]))
 spectra_raw = apply(spectra_raw, 2, as.numeric)
-spectra_meta = raw[,c(766:773)]
+spectra_meta = raw[-c(1,2),c(766:773)]
+
+ids = stri_rand_strings(nrow(spectra_meta),5)
+# Check IDs for duplicates
+while (sum(duplicated(ids))!=0) {
+  ids = stri_rand_strings(nrow(spectra_meta),5)
+}
+
+spectra_meta$ids = ids
+rownames(spectra_raw) = ids
+
+DAL = spectra_meta
+DAL$raw = spectra_raw
+DAL = DAL[,c("ids","type","species","depth","raw")]
+
+saveRDS(DAL, file = here("data","output","DAL_spectra.rds"))
