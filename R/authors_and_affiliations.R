b34e9afbe58f3b937213580608cc8f80ee962c55
Florian <florian.muthreich@uib.no>
2020-12-03 13:36:21 +0100

    read_data file; loading core data chunk; added authors and affiliations

---
+++ R/authors_and_affiliations.R
@@ -0,0 +1,73 @@
+library(tidyr)
+library(dplyr)
+
+dat <- data.frame(FirstName = c("Florian", 
+                                "Valeria", 
+                                "Boris", 
+                                "Alistair W R"),
+                  LastName = c("Muthreich", 
+                               "Tafintseva", 
+                               "Zimmermann", 
+                               "Seddon"),
+                  Affiliation1 = c("University of Bergen, Department of Biological Sciences, Norway", 
+                                   "University of Life Sciences, Ås, Norway",
+                                   "University of Life Sciences, Ås, Norway",
+                                   "University of Bergen, Department of Biological Sciences, Norway"),
+                  Affiliation2 = c(NA, 
+                                   NA, 
+                                   NA, 
+                                   "Bjerknes Centre for Climate Research, Bergen"))
+dat <- dat %>% 
+  # gather all affiliations in one column
+  gather(key = Number, value = Affiliation, Affiliation1, Affiliation2) %>%
+  # remove rows with no Affiliations
+  dplyr::filter(!is.na(Affiliation))
+
+# Function to get affiliations ranked from 1 to n (this function was found on Stack Overflow)
+rankID <- function(x){
+  su=sort(unique(x))
+  for (i in 1:length(su)) x[x==su[i]] = i
+  return(x)
+}
+
+
+NameAffList <- dat %>% 
+  dplyr::arrange(LastName, Affiliation) %>% 
+  rowwise() %>% 
+  # extract the first letter of each first name and put a dot after each letter
+  mutate(
+    Initials = paste(stringi::stri_extract_all(regex = "\\b([[:alpha:]])", str = FirstName, simplify = TRUE), collapse = ". "),
+    Initials = paste0(Initials, ".")) %>%
+  ungroup() %>% 
+  # add a column from 1 to n
+  mutate(ID = 1:n()) %>%
+  group_by(Affiliation) %>% 
+  # replace ID with min number (same affiliations become the same number)
+  mutate(ID = min(ID)) %>% 
+  ungroup() %>% 
+  # use function above to assign new ID from 1 to n
+  mutate(ID = rankID(ID)) %>%
+  #Paste Last and Initials
+  mutate(name = paste0(LastName, ", ", Initials)) %>%
+  arrange(LastName, ID)
+
+#NameAffList$ID = factor(NameAffList$ID)
+
+Authors = NameAffList %>%   
+  group_by(LastName, name) %>% 
+  summarise(affs = paste(ID, collapse = ",")) %>% 
+  mutate(
+    affs = paste0("^", affs, "^"),
+    nameID = paste0(name, affs))
+Authors = Authors[match(unique(dat$LastName),Authors$LastName),] %>%
+  #arrange(unique(dat$LastName)) %>%
+  pull(nameID) %>% 
+  paste(collapse = ", ")
+
+Affiliations = NameAffList %>% 
+  distinct(ID, Affiliation) %>% 
+  arrange(ID) %>% 
+  mutate(ID = paste0("^", ID, "^")) %>% 
+  mutate(Affiliation2 = paste(ID, Affiliation, sep = "")) %>% 
+  pull(Affiliation2) %>% 
+  paste(collapse = ", ")
--- R/read_data.R
+++ R/read_data.R
@@ -9,33 +9,30 @@ repl = function(data, c){
   return(tmp)
 }
 
-# assign ids
-# assign metadata
-# save as rds
-
-raw = read.csv(here("data", "input", "spectra", "DAL_spectra_raw.csv"), check.names = F, colClasses = "character")
-
-spectra_raw = as.matrix((raw[-c(1,2),c(1:765)]))
-spectra_raw = apply(spectra_raw, 2, as.numeric)
-spectra_meta = raw[-c(1,2),c(766:ncol(raw))]
-sc = strtrim(spectra_meta$`Sample Name`[1], 3)
-ids = paste0(sc,stri_rand_strings(nrow(spectra_meta),5))
-
-# Check IDs for duplicates
-while (sum(duplicated(ids))!=0) {
+read_data = function(sc){
+  raw = read.csv(here("data", "input", "spectra", paste0(sc,"_spectra_raw.csv")), 
+                 check.names = F, 
+                 colClasses = "character")
+  spectra_raw = as.matrix((raw[-c(1,2),c(1:765)]))
+  spectra_raw = apply(spectra_raw, 2, as.numeric)
+  spectra_meta = raw[-c(1,2),c(766:ncol(raw))]
+  sc = strtrim(spectra_meta$`Sample Name`[1], 3)
   ids = paste0(sc,"_",stri_rand_strings(nrow(spectra_meta),5))
+  
+  # Check IDs for duplicates
+  while (sum(duplicated(ids))!=0) {
+    ids = paste0(sc,"_",stri_rand_strings(nrow(spectra_meta),5))
+  }
+  
+  spectra_meta$ids = ids
+  spectra_meta$species = repl(spectra_meta, c("Blank", "Pinus", "Abies", "BetCor", "Alnus"))
+  spectra_meta$treatment = repl(spectra_meta, c("acet"))
+  spectra_meta$orientation = repl(spectra_meta,c("_side"))
+  
+  rownames(spectra_raw) = ids
+  
+  data = spectra_meta
+  data$raw = spectra_raw
+  
+  saveRDS(data, file = here("data","output",paste0(sc,"_spectra.rds")))
 }
-
-spectra_meta$ids = ids
-spectra_meta$species = repl(DAL, c("Blank", "Pinus", "Abies", "BetCor", "Alnus"))
-spectra_meta$treatment = repl(DAL, c("acet"))
-spectra_meta$orientation = repl(DAL,c("_side"))
-
-rownames(spectra_raw) = ids
-
-DAL = spectra_meta
-DAL$raw = spectra_raw
-#DAL = DAL[,c("ids","type","species","depth","raw")]
-
-
-saveRDS(DAL, file = here("data","output",paste0(sc,"_spectra.rds")))
