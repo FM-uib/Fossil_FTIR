8837a059958cba6a66d5a59639f552f5759e556f
Florian <florian.muthreich@uib.no>
2020-12-07 23:27:04 +0100

    preproc function cuts spectra and carries row/colnames

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,8 +1,15 @@
-pre_processing = function(spectra, ws){
-  foo_sg = SavitzkyGolay(spectra, poly = 2, width = 5, deriv = 2)
+pre_processing = function(spectra, ws, cut=T){
+  wn = as.numeric(colnames(spectra))
+  foo_sg = SavitzkyGolay(spectra, poly = 2, width = ws, deriv = 2)
+  if (cut) {
+    foo_sg = foo_sg[,wn<1900 & wn>700]
+    colnames(foo_sg) = wn[wn<1900 & wn>700]
+  } else {
+    colnames(foo_sg) = wn
+  }
   foo_emsc = EMSC(foo_sg, degree = 1)$corrected
   rownames(foo_emsc) = rownames(spectra)
-  colnames(foo_emsc) = colnames(spectra)
+  colnames(foo_emsc) = colnames(foo_sg)
   return(foo_emsc)
 }
 
