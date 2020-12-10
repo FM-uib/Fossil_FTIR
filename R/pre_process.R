aeac5cb14faee208b3eb1000449a182892b75c34
Florian <florian.muthreich@uib.no>
2020-12-10 23:22:38 +0100

    wrote preprocess file and hook into manuscript

---
+++ R/pre_process.R
@@ -0,0 +1,22 @@
+pre_processing = function(spectra, ws, cut=T){
+  wn = as.numeric(colnames(spectra))
+  foo_sg = SavitzkyGolay(spectra, poly = 2, width = ws, deriv = 2)
+  if (cut) {
+    foo_sg = foo_sg[,wn<1900 & wn>700]
+    colnames(foo_sg) = wn[wn<1900 & wn>700]
+  } else {
+    colnames(foo_sg) = wn
+  }
+  foo_emsc = EMSC(foo_sg, degree = 1)$corrected
+  rownames(foo_emsc) = rownames(spectra)
+  colnames(foo_emsc) = colnames(foo_sg)
+  return(foo_emsc)
+}
+
+DAL$sg2 = pre_processing(DAL$raw, 6)
+TSK$sg2 = pre_processing(TSK$raw, 6)
+MFM$sg2 = pre_processing(MFM$raw, 6)
+
+saveRDS(DAL, here("data","output","DAL_spectra.rds"))
+saveRDS(TSK, here("data","output","TSK_spectra.rds"))
+saveRDS(MFM, here("data","output","DAL_spectra.rds"))
