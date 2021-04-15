f6b82352444b33a15eb9dc4cc970b80015c7455f
Florian <florian.muthreich@uib.no>
2021-04-15 22:02:11 +0200

    added Als comments; wrote pca bootstrap to test orientation and depths

--- R/pca.R
+++ R/pca.R
@@ -3,16 +3,19 @@ pca_wrap = function(pca_data,sel){
   if (is.expression(sel)) {
     pca_data = subset(pca_data, eval(sel))
   }
-  pca_res = prcomp(pca_data$sg2)
+  pca_res = prcomp(pca_data$sg2, center = T, scale. = F)
   l = list()
-  l[["scores"]] =  as.data.frame(pca_res$x[,1:5]) %>%
+  l[["pca"]] = pca_res
+  l[["scores"]] =  as.data.frame(pca_res$x[,1:10]) %>%
     mutate(ids = rownames(.),
            core = pca_data$Label,
            treatment = pca_data$treatment,
            depth = pca_data$depth,
-           age = pca_data$age
+           age = pca_data$age,
+           orientation = pca_data$orientation,
+           type = pca_data$type
            )
-  l[["loadings"]] = as.data.frame(pca_res$rotation[,1:5])
+  l[["loadings"]] = as.data.frame(pca_res$rotation[,1:10])
   return(l)
 }
 
@@ -28,24 +31,129 @@ acet_sel = function(data){
 pca_data = rbind(FRE,
                  acet_sel(DAL),
                  acet_sel(TSK),
-                 acet_sel(MFM))
+                 acet_sel(MFM)
+                 )
 
 l = pca_wrap(pca_data, "")
 ggplot(l[["scores"]], aes(PC1, PC2, color = core)) + geom_point(aes(shape = treatment), size = 4)
 
+ggplot(l[["loadings"]], aes(as.numeric(rownames(l[["loadings"]])),PC1)) + geom_line()
+ggplot(l[["loadings"]], aes(as.numeric(rownames(l[["loadings"]])),PC2)) + geom_line()
+
+
 # Cores SPT only 
-pca_data = rbind(FRE,
+pca_data = rbind(
                  filter(DAL, treatment == "SPT" & species == "Pinus"),
                  filter(TSK, treatment == "SPT" & species == "Pinus"),
-                 filter(MFM, treatment == "SPT" & species == "Pinus"))
+                 filter(MFM, treatment == "SPT" & species == "Pinus")
+                 )
+
+l = pca_wrap(pca_data, "")
+ggplot(l[["scores"]], aes(PC1, PC2, color = depth)) + geom_point(aes(shape = treatment), size = 4)
+
+ggplot(l[["loadings"]], aes(as.numeric(rownames(l[["loadings"]])),PC1)) + geom_line()
+ggplot(l[["loadings"]], aes(as.numeric(rownames(l[["loadings"]])),PC2)) + geom_line()
 
 # Cores acet only 
-pca_data = rbind(FRE,
+pca_data = rbind(
                  filter(DAL, treatment == "acet" & species == "Pinus"),
                  filter(TSK, treatment == "acet" & species == "Pinus"),
-                 filter(MFM, treatment == "acet" & species == "Pinus"))
+                 filter(MFM, treatment == "acet" & species == "Pinus")
+                 )
+
+l = pca_wrap(pca_data, "")
+ggplot(l[["scores"]], aes(PC1, PC2, color = core)) + geom_point(aes(shape = orientation), size = 4)
+
+ggplot(l[["loadings"]], aes(as.numeric(rownames(l[["loadings"]])),PC1)) + geom_line()
+ggplot(l[["loadings"]], aes(as.numeric(rownames(l[["loadings"]])),PC2)) + geom_line()
 
 
+
+#show difference between acetolyzed and non
+#show depth differences
+#show difference to fresh
+#show difference in orientation fresh vs spt vs acet
+# pca on each core with lda
+
+#LDAs
+#Orientation
+
+bootstrap_pca = function(data, target = "orientation", n = 50){
+  res = rep(0,n)
+  for (i in 1:n) {
+    train = runif(length(data[,target])) <=.7
+    while (sum(train) == length(train)) {
+      train = runif(length(data[,target])) <=.7
+    }
+    
+  #PCA
+    l = pca_wrap(data[train, ],"")
+  #LDA
+    z = lda(as.formula(paste(target,"~ PC1 + PC2")) , l[["scores"]])
+    res[i] =sum(predict(z, as.data.frame(predict(l[["pca"]],data[!train,"sg2"]))[,1:2])$class == data[!train,target])/length(data[!train, target])
+    
+  }
+
+  return(mean(res))
+}
+
+pca_lda = data.frame(core = c("FRE","DAL","DAL","TSK","TSK","MFM","MFM"),
+                     treatment = c("fresh","SPT","acet","SPT","acet","SPT","acet"),
+                     acc = c(bootstrap_pca(FRE),
+                             bootstrap_pca(filter(DAL,treatment == "SPT", species == "Pinus")),
+                             bootstrap_pca(filter(DAL,treatment == "acet", species == "Pinus")),
+                             bootstrap_pca(filter(TSK,treatment == "SPT", species == "Pinus")),
+                             bootstrap_pca(filter(TSK,treatment == "acet", species == "Pinus")),
+                             bootstrap_pca(filter(MFM,treatment == "SPT", species == "Pinus")),
+                             bootstrap_pca(filter(MFM,treatment == "acet", species == "Pinus"))
+                             )*100,
+                     n = c(nrow(FRE),
+                           nrow(filter(DAL,treatment == "SPT", species == "Pinus")),
+                           nrow(filter(DAL,treatment == "acet", species == "Pinus")),
+                           nrow(filter(TSK,treatment == "SPT", species == "Pinus")),
+                           nrow(filter(TSK,treatment == "acet", species == "Pinus")),
+                           nrow(filter(MFM,treatment == "SPT", species == "Pinus")),
+                           nrow(filter(MFM,treatment == "acet", species == "Pinus"))
+                           )
+                     )
+
+pca_lda_depth = data.frame(core = c("FRE","DAL","DAL","TSK","TSK","MFM","MFM"),
+                     treatment = c("fresh","SPT","acet","SPT","acet","SPT","acet"),
+                     acc = c(bootstrap_pca(FRE, target = "type"),
+                             bootstrap_pca(filter(acet_sel(DAL),treatment == "SPT", species == "Pinus"), target = "depth"),
+                             bootstrap_pca(filter(acet_sel(DAL),treatment == "acet", species == "Pinus"), target = "depth"),
+                             bootstrap_pca(filter(acet_sel(TSK),treatment == "SPT", species == "Pinus"), target = "depth"),
+                             bootstrap_pca(filter(acet_sel(TSK),treatment == "acet", species == "Pinus"), target = "depth"),
+                             bootstrap_pca(filter(acet_sel(MFM),treatment == "SPT", species == "Pinus"), target = "depth"),
+                             bootstrap_pca(filter(acet_sel(MFM),treatment == "acet", species == "Pinus"), target = "depth")
+                     )*100,
+                     n = c(nrow(FRE),
+                           nrow(filter(acet_sel(DAL),treatment == "SPT", species == "Pinus")),
+                           nrow(filter(acet_sel(DAL),treatment == "acet", species == "Pinus")),
+                           nrow(filter(acet_sel(TSK),treatment == "SPT", species == "Pinus")),
+                           nrow(filter(acet_sel(TSK),treatment == "acet", species == "Pinus")),
+                           nrow(filter(acet_sel(MFM),treatment == "SPT", species == "Pinus")),
+                           nrow(filter(acet_sel(MFM),treatment == "acet", species == "Pinus"))
+                     )
+)
+
+pca_data = filter(DAL,treatment == "acet", species == "Pinus")
+train = runif(length(pca_data$orientation)) <=.7
+
+l = pca_wrap(pca_data[train,], "")
+
+z = lda(orientation ~ PC1 + PC2, l[["scores"]])
+sum(predict(z, as.data.frame(predict(l[["pca"]],pca_data[!train,"sg2"]))[,1:2])$class == pca_data$orientation[!train])/length(pca_data$orientation[!train])
+
+
+
+# LDA
+z = lda(orientation ~ PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10, l[["scores"]], CV = T)
+
+sum(z$class == l[["scores"]]$orientation)/length(z$class)
+
+
+#old
 pca_data = rbind(subset(DAL, species == "Pinus"), 
                  subset(TSK, species == "Pinus"), 
                  subset(MFM, species == "Pinus"))
