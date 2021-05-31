f0ec5353238444a05bf12c0951f4545d725e1fb7
Florian <florian.muthreich@uib.no>
2021-05-31 14:04:18 +0200

    core for regression coeff using plsr

--- R/Regression.R
+++ R/Regression.R
@@ -2,6 +2,50 @@
 
 library(pls)
 
+FRE$treat = "fresh"
+DAL$treat = "fossil"
+MFM$treat = "fossil"
+TSK$treat = "fossil"
+
+# PLS fresh vs fossil
+
+pls_data = filter(rbind(FRE,DAL,MFM,TSK), species == "Pinus")
+pls_data$y = model.matrix(~treatment -1, data = pls_data)
+colnames(pls_data$sg2) = colnames(DAL$sg2)
+
+results = plsr(y ~sg2, 6, data = pls_data, validation = "LOO")
+plot(results, plottype = "coefficients")
+
+# PLS SPT vs acet
+## all cores
+pls_data = filter(
+  rbind(
+    acet_sel(DAL),
+    acet_sel(MFM),
+    acet_sel(TSK)
+    ), 
+  species == "Pinus")
+pls_data$y = model.matrix(~treatment -1, data = pls_data)
+colnames(pls_data$sg2) = colnames(DAL$sg2)
+
+results = plsr(y ~sg2, 6, data = pls_data, validation = "LOO")
+plot(results, plottype = "coefficients")
+
+## DAL
+pls_data = filter(
+  rbind(
+    acet_sel(DAL)
+  ), 
+  species == "Pinus")
+pls_data$y = model.matrix(~treatment -1, data = pls_data)
+colnames(pls_data$sg2) = colnames(DAL$sg2)
+
+results = plsr(y ~sg2, 6, data = pls_data, validation = "LOO")
+plot(results, plottype = "coefficients")
+
+
+
+## 
 pca_data = rbind(
                  acet_sel(DAL),
                  acet_sel(TSK),
